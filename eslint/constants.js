67626314bf57c2539f8d4d7011b12ef08c6036a1
Pascal Weiland <weiland@users.noreply.github.com>
2025-03-04 19:10:58 +0100

    feat: upgrade ecma version to `es2025`

--- eslint/constants.js
+++ eslint/constants.js
@@ -1,4 +1,4 @@
-export const ECMA_VERSION = 2022
+export const ECMA_VERSION = 2025
 export const GLOB_SRC_EXT = '?([cm])[jt]s?(x)'
 export const GLOB_JS = '**/*.?([cm])js'
 export const JAVASCRIPT_FILES = ['*.js?(x)', '*.mjs']
