115cb59afdb211aed4b0ef7712a605436262daf2
Pascal Weiland <weiland@users.noreply.github.com>
2025-09-03 15:11:24 +0200

    chore: update readme instructions

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,14 +2,9 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
 	entry: {
-		// 'biome.json': 'src/biome/biome.json',
 		index: 'src/index.ts',
 		eslint: 'src/eslint/index.ts',
 		prettier: 'src/prettier/index.js',
-		// copies everything from `biome/` to `dist/
-		// publicDir: 'src/biome'
 	},
-	// loader: {
-	// 	'.json': 'copy',
-	// },
+	copy: ['src/biome/biome.json', { from: 'src/biome/biome.json', to: 'dist/biome.json' }],
 })
