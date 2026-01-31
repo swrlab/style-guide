796d083d94335758d971e758bdd5af63ceddfb22
Pascal Weiland <weiland@users.noreply.github.com>
2026-01-31 14:55:52 +0100

    fix(tsdown): fix copying biome.json

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
 		eslint: 'src/eslint/index.ts',
 		prettier: 'src/prettier/index.js',
 	},
-	copy: ['src/biome/biome.json', { from: 'src/biome/biome.json', to: 'dist/biome.json' }],
+	copy: ['src/biome/biome.json', { from: 'src/biome/biome.json', to: 'dist' }],
 })
